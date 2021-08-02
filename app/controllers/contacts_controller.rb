1b5d19aed70196ea6d345a289fc93926f9e8637b
moeka kitamura <moeka.k76@gmail.com>
2021-08-02 22:28:35 +0900

    add private method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,6 +11,7 @@ class ContactsController < ApplicationController
     end
   end
 
+  private
   def contact_params
     params.require(:contact).permit(:name, :email, :content)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # contacts POST /contacts(.:format) contacts#create
+  # new_contact GET  /contacts/new(.:format) contacts#new
 end
